33a95f88f51861da68b8de3878fe5dcd20264d1b
Nico Tosco <tosconicolas@gmail>
2022-05-01 14:28:18 -0300

    replace App as root and make BEM style

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import './App.scss';
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       
+    <div className="root">
+      <header className="root__header">
+        <span>This is header</span>
       </header>
-      <main>
+      <main className="root__main">
         <h1>This is a new app</h1>
       </main>
     </div>
